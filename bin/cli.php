f3f9e477eedbb88f00e5dc183d37340416e058d9
Duane Jeffers <duane@duanejeffers.com>
2015-02-25 20:23:27 -0600

    cli code and update to composer

--- bin/cli.php
+++ bin/cli.php
@@ -2,6 +2,8 @@
 	// BRMManager - cli application
 	include('../vendor/autoload.php');
 
+	define('DATABASE_FILE', '../data/database.db');
+
 	$cli = new Cli(array(
 		'install' => "Creates the database for application",
 		'reset' => "Deletes and Recreates the Database when the --install flag is used",
@@ -12,13 +14,23 @@
 	$cli->print_help('h', 'BRM Manager Cli Admin', TRUE);
 
 	if($cli->opt('install')) {
+		// Check to see if file exists in the filesystem.
+		if(is_file(DATABASE_FILE) && !$cli->opt('reset')) {
+			$cli->print_exit('Database file exists. Please use --reset if you want to replace this file.');
+		} elseif(is_file(DATABASE_FILE) && $cli->opt('reset')) {
+			unlink(DATABASE_FILE);
+		}
 
+		\ORM::configure('sqlite:'. DATABASE_FILE);
+
+		
 	}
 
 	if($cli->opt('createadmin')) {
 		// Specify the Database and then add User.
-
-		\ORM::configure('sqlite:../data/database.db');
+		if(!$cli->opt('install')) {
+			\ORM::configure('sqlite:'. DATABASE_FILE);
+		}
 
 		$firstname = $cli->print_read('First Name:');
 		$lastname = $cli->print_read('Last Name:');
