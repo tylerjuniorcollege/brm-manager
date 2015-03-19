5d5226e993f3c0290a7947ebd453874cf4c5af6e
Duane Jeffers <duane@duanejeffers.com>
2015-03-19 15:35:09 -0500

    Added User and Department Linking.

--- app/src/Model/BRM/Request.php
+++ app/src/Model/BRM/Request.php
@@ -9,8 +9,16 @@ class Request
 {
 	public static $_table = 'brm_requests';
 
+	public function user() {
+		return $this->has_one('User', 'id', 'userid')->find_one();
+	}
+
 	public function addUser(User $user) {
 		$this->userid = $user->id;
+	}
+
+	public function department() {
+		return $this->has_one('Department', 'id', 'departmentid')->find_one();
 	}
 
 	public function addDepartment(Department $department) {
